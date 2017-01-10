af272044a84a35e568e4155a3e182258a2c01d78
tnasergey2 <tnasergey@yandex.ua>
2017-01-10 04:06:49 +0200

    Add window WinLogin

--- WinLogin/MainWindow.xaml.cs
+++ WinLogin/MainWindow.xaml.cs
@@ -23,6 +23,77 @@ namespace WinLogin
         public MainWindow()
         {
             InitializeComponent();
+
+            labelFIO.Visibility = Visibility.Collapsed;
+            labelGroup.Visibility = Visibility.Collapsed;
+            labelLogin.Visibility = Visibility.Collapsed;
+            labelPasswordTech.Visibility = Visibility.Collapsed;
+            labelPasswordAdm.Visibility = Visibility.Collapsed;
+            textBoxFIO.Visibility = Visibility.Collapsed;
+            textBoxGroup.Visibility = Visibility.Collapsed;
+            textBoxLogin.Visibility = Visibility.Collapsed;
+            textBoxPasswordTech.Visibility = Visibility.Collapsed;
+            textBoxPasswordAdm.Visibility = Visibility.Collapsed;
+            buttonLogin.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void buttonStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+            labelFIO.Visibility = Visibility.Visible;
+            labelGroup.Visibility = Visibility.Visible;
+            labelLogin.Visibility = Visibility.Collapsed;
+            labelPasswordTech.Visibility = Visibility.Collapsed;
+            labelPasswordAdm.Visibility = Visibility.Collapsed;
+            textBoxFIO.Visibility = Visibility.Visible;
+            textBoxGroup.Visibility = Visibility.Visible;
+            textBoxLogin.Visibility = Visibility.Collapsed;
+            textBoxPasswordTech.Visibility = Visibility.Collapsed;
+            textBoxPasswordAdm.Visibility = Visibility.Collapsed;
+            buttonLogin.Visibility = Visibility.Visible;
+
+        }
+
+        private void buttonTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            labelFIO.Visibility = Visibility.Collapsed;
+            labelGroup.Visibility = Visibility.Collapsed;
+            labelLogin.Visibility = Visibility.Visible;
+            labelPasswordTech.Visibility = Visibility.Visible;
+            labelPasswordAdm.Visibility = Visibility.Collapsed;
+            textBoxFIO.Visibility = Visibility.Collapsed;
+            textBoxGroup.Visibility = Visibility.Collapsed;
+            textBoxLogin.Visibility = Visibility.Visible;
+            textBoxPasswordTech.Visibility = Visibility.Visible;
+            textBoxPasswordAdm.Visibility = Visibility.Collapsed;
+            buttonLogin.Visibility = Visibility.Visible;
+
+        }
+
+        private void buttonAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            labelFIO.Visibility = Visibility.Collapsed;
+            labelGroup.Visibility = Visibility.Collapsed;
+            labelLogin.Visibility = Visibility.Collapsed;
+            labelPasswordTech.Visibility = Visibility.Collapsed;
+            labelPasswordAdm.Visibility = Visibility.Visible;
+            textBoxFIO.Visibility = Visibility.Collapsed;
+            textBoxGroup.Visibility = Visibility.Collapsed;
+            textBoxLogin.Visibility = Visibility.Collapsed;
+            textBoxPasswordTech.Visibility = Visibility.Collapsed;
+            textBoxPasswordAdm.Visibility = Visibility.Visible;
+            buttonLogin.Visibility = Visibility.Visible;
+
+        }
+
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("It is work!");
+            
+
+
         }
     }
 }
