5a013a92779cdacd75e9d4605cffb4c34ffa0ce3
tnasergey2 <tnasergey@yandex.ua>
2017-01-19 05:01:25 +0200

    19.01.2017

--- WinLogin/WinAdmin.xaml.cs
+++ WinLogin/WinAdmin.xaml.cs
@@ -21,32 +21,46 @@ namespace WinLogin
     public partial class WinAdmin : Window
     {
         ExamTicket_dbEntities db;
+
+        Teachers tch = new Teachers();
+
+        SubjectOfTeacher sot = new SubjectOfTeacher();
         public WinAdmin()
         {
             InitializeComponent();
 
             textBox_id_teacher.IsReadOnly = true;
+            textBox_id_subject.IsReadOnly = true;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             db = new ExamTicket_dbEntities();
-            Load();
+            Load_tch();
+            Load_sub();
         }
-        private void Load()
+        private void Load_tch()
         {
             db.Teachers.Load();
             dataGrid_teachers.ItemsSource = db.Teachers.Local.ToBindingList();
+
+
+
+            //if (tch.id_teacher == sot.id_teacher_sot)
+            //{
+            //    listBox_Subjects_Of_Techers.Items[0] = sot.id_subject_sot;
+            //}
+                
         }
-        private void Save()
+        private void Save_tch()
         {
             db.SaveChanges();
-            Load();
+            Load_tch();
         }
         private void Add_Teacher_Click(object sender, RoutedEventArgs e)
         {
             
-            Teachers tch = new Teachers();
+            //Teachers tch = new Teachers();
             int id = db.Teachers.Select(t => t.id_teacher).Max() + 1;
             tch.id_teacher = id;
             db.Teachers.Add(tch);
@@ -56,7 +70,7 @@ namespace WinLogin
 
         private void Save_Teacher_Click(object sender, RoutedEventArgs e)
         {
-            Save();
+            Save_tch();
         }
 
         private void Del_Teacher_Click(object sender, RoutedEventArgs e)
@@ -64,7 +78,7 @@ namespace WinLogin
             if (MessageBox.Show("Вы действительно хотите удалить запись?", "Предупреждение", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
             {
                 db.Teachers.Remove((Teachers)dataGrid_teachers.SelectedItem);
-                Save();
+                Save_tch();
             }
         }
 
@@ -80,6 +94,42 @@ namespace WinLogin
             else
                 dataGrid_teachers.ItemsSource = db.Teachers.Local.ToBindingList();
             //Поиск
+        }
+
+
+        ///*******************************************************************************************************
+        private void Load_sub()
+        {
+            db.Subjects.Load();
+            dataGrid_Subjects.ItemsSource = db.Subjects.Local.ToBindingList();
+        }
+        private void Save_sub()
+        {
+            db.SaveChanges();
+            Load_sub();
+        }
+        
+        private void Add_Subject_Click(object sender, RoutedEventArgs e)
+        {
+            Subjects sub = new Subjects();
+            int id = db.Subjects.Select(t => t.id_subject).Max() + 1;
+            sub.id_subject = id;
+            db.Subjects.Add(sub);
+            dataGrid_Subjects.SelectedIndex = dataGrid_Subjects.Items.Count - 1;
+        }
+
+        private void Save_Subject_Click(object sender, RoutedEventArgs e)
+        {
+            Save_sub();
+        }
+
+        private void Del_Subject_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите удалить запись?", "Предупреждение", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                db.Subjects.Remove((Subjects)dataGrid_Subjects.SelectedItem);
+                Save_sub();
+            }
         }
     }
 }
